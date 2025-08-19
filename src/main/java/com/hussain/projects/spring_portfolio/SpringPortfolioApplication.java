54473b8fd4d2c0ae415853963ca0cb83c06621de
Munkybooty <shrunkymunky@icloud.com>
2025-08-18 22:45:10 -0400

    chore(config): add .env support, example file provided

--- src/main/java/com/hussain/projects/spring_portfolio/SpringPortfolioApplication.java
+++ src/main/java/com/hussain/projects/spring_portfolio/SpringPortfolioApplication.java
@@ -1,5 +1,6 @@
 package com.hussain.projects.spring_portfolio;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringPortfolioApplication {
 
 	public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+
+        // Push .env variables into system properties for Spring to resolve
+        dotenv.entries().forEach(entry ->
+                System.setProperty(entry.getKey(), entry.getValue())
+        );
+
 		SpringApplication.run(SpringPortfolioApplication.class, args);
 	}
 
